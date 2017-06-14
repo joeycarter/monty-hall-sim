1dfdeb8b83d88462b7bb6ab01409fb793fb98f7c
Joey Carter <joey.snarrcarter@gmail.com>
2017-06-13 22:20:26 -0400

    Changed output format; added documentation

--- README.md
+++ README.md
@@ -1,8 +1,54 @@
 # A Monty Hall Simulation
 
-A Python script to simulate the age-old Monty Hall Problem to convince myself than you *are* in fact more likely to win if you switch doors.
+The Monty Hall Problem is a simple game of chance whose counterintuitive "solution" has bothered a number of otherwise mathematically literate people I've met over the years.
+
+Here's a short description of the game from [Wikipedia](https://en.wikipedia.org/wiki/Monty_Hall_problem):
+
+> Suppose you're on a game show, and you're given the choice of three doors: behind one door is a car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you, 'Do you want to pick door No. 2?' The puzzle is, is it to your advantage to switch your choice?
+
+Rules of probability say that players who switch have a 2/3 chance of winning the car, while players who stick to their initial choice have only a 1/3 chance.
+
+I don't doubt that the math checks out, but I wanted a simple simulation to convince myself once and for all that you *are* in fact more likely to win if you switch doors.
+
+## Dependencies
+
+* Python 3 (tested with v3.5.2)
+* NumPy (tested with v1.12.0)
 
 ## Usage
 
+To run the Python script with default parameters:
+
     python monty-hall.py
 
+To run e.g. N=10 trials, with verbose output:
+
+    python monty-hall.py -v -N 10
+
+For more information:
+
+    python mony-hall.py --help
+
+## Example
+
+```bash
+$ python monty-hall.py -N 100000
+```
+
+```
+Always changing doors
+Running simulation with N = 100000 trials...
+
+Number of wins = 66711
+Fraction of games won: 0.66711
+
+----------
+
+Always staying
+Running simulation with N = 100000 trials...
+
+Number of wins = 33351
+Fraction of games won: 0.33351
+```
+
+It works!
--- monty-hall.py
+++ monty-hall.py
@@ -1,100 +1,185 @@
+"""monty-hall.py
+
+Author: Joey Carter
+
+"""
+
+import argparse
 import numpy as np
+import warnings
+
+# Parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="verbose output", action="store_true")
+parser.add_argument("-N", "--ntrial", type=int, default=100, help="number of trials to run")
+args = parser.parse_args()
 
 # Number of trials
-N = 100000
+N = args.ntrial
 
-# Door the car is behind (1, 2, 3)
-cars = np.random.randint(1, 4, size=N)
-
-# Player's initial selection (1, 2, 3)
-initSels = np.random.randint(1, 4, size=N)
+if args.verbose and N >= 200:
+    warnings.warn("This will produce a lot of output!")
+    input("Press Enter to continue...")
 
 
 def trial(car, initSel, change):
-	# print('Player chooses door ' + str(initSel) + ' (car behind door ' + str(car) + ')')
+    """Perform the simulation of one trial
 
-	# Open one of the doors without a car
-	tmp = np.random.randint(2)
+    Parameters
+    ----------
+    car : int
+        The door the car is behind (1, 2 or 3)
+    initSel : int
+        The player's initial door selection (1, 2 or 3)
+    change : bool
+        If true, the player will change their selection when asked
 
-	# Player guesses; reveal a door with a goat behind it
-	if car == initSel:
-		# Player guessed correctly; open either of the other doors
-		reveal = randExclude(car)
-	else:
-		# Player guessed incorrectly; open the other goat door
-		reveal = exclude(car, initSel)
+    Returns
+    -------
+    bool
+        True if the player wins, False if they lose
+    """
 
-	# print('Door ' + str(reveal) + ' is revealed')
+    if args.verbose:
+        print('Player chooses door ' + str(initSel) +
+              ' (car behind door ' + str(car) + ')')
 
-	if change:
-		finalSel = exclude(reveal, initSel)
-		# print('Player decides to change to door ' + str(finalSel))
-	else:
-		finalSel = initSel
-		# print('Player stays with door ' + str(finalSel))
+    # Player guesses; reveal a door with a goat behind it
+    if car == initSel:
+        # Player guessed correctly; open either of the other doors
+        reveal = randExclude(car)
+    else:
+        # Player guessed incorrectly; open the other goat door
+        reveal = exclude(car, initSel)
 
-	# Check if they won
-	if car == finalSel:
-		win = 1
-		# print('Player wins!\n')
-	else:
-		win = 0
-		# print('Player looses!\n')
+    if args.verbose:
+        print('Door ' + str(reveal) + ' is revealed')
 
-	return win
+    if change:
+        finalSel = exclude(reveal, initSel)
+        if args.verbose:
+            print('Player decides to change to door ' + str(finalSel))
+    else:
+        finalSel = initSel
+        if args.verbose:
+            print('Player stays with door ' + str(finalSel))
+
+    # Check if they won
+    if car == finalSel:
+        win = True
+        if args.verbose:
+            print('Player wins!\n')
+    else:
+        win = False
+        if args.verbose:
+            print('Player looses!\n')
+
+    return win
 
 
 def randExclude(ex):
-	"""Generate a random number on interval [1,3], excluding 'ex'
-	"""
-	
-	tmp = np.random.randint(2)
+    """Generate a random number on interval [1,3], excluding 'ex'
 
-	if ex == 1:
-		if tmp == 0:
-			return 2
-		else:
-			return 3
-	elif ex == 2:
-		if tmp == 0:
-			return 1
-		else:
-			return 3
-	elif ex == 3:
-		if tmp == 0:
-			return 1
-		else:
-			return 2
-	else:
-		raise Exception('randExclude only works for 1, 2, 3')
+    Parameters
+    ----------
+    ex : int
+        The number to exclude (1, 2 or 3)
+
+    Returns
+    -------
+    int
+        The random number
+    """
+
+    tmp = np.random.randint(2)
+
+    if ex == 1:
+        if tmp == 0:
+            return 2
+        else:
+            return 3
+    elif ex == 2:
+        if tmp == 0:
+            return 1
+        else:
+            return 3
+    elif ex == 3:
+        if tmp == 0:
+            return 1
+        else:
+            return 2
+    else:
+        raise Exception('randExclude only works for 1, 2, 3')
 
 
 def exclude(ex1, ex2):
-	"""Return the number on the interval [1,3] that is neither ex1 nor ex2
-	"""
+    """Return the number on the interval [1,3] that is neither ex1 nor ex2
 
-	if (ex1 == 1 and ex2 == 2) or (ex1 == 2 and ex2 == 1):
-		return 3
-	elif (ex1 == 1 and ex2 == 3) or (ex1 == 3 and ex2 == 1):
-		return 2
-	elif (ex1 == 2 and ex2 == 3) or (ex1 == 3 and ex2 == 2):
-		return 1
-	else:
-		raise Exception('exclude only works for 1, 2, 3')
+    Parameters
+    ----------
+    ex1 : int
+        The first number to exclude (1, 2 or 3)
+    ex2 : int
+        The second number to exclude (1, 2 or 3)
 
-# Run N trials
+    Returns
+    -------
+    int
+        The number (1, 2 or 3) not excluded
+    """
 
-nWins = 0
+    if (ex1 == 1 and ex2 == 2) or (ex1 == 2 and ex2 == 1):
+        return 3
+    elif (ex1 == 1 and ex2 == 3) or (ex1 == 3 and ex2 == 1):
+        return 2
+    elif (ex1 == 2 and ex2 == 3) or (ex1 == 3 and ex2 == 2):
+        return 1
+    else:
+        raise Exception('exclude only works for 1, 2, 3')
 
-for i in range(0, N):
-	win = trial(cars[i], initSels[i], 1)
-	
-	if win:
-		nWins += 1
 
-print('N = ' + str(N) + ', N wins = ' + str(nWins))
+def runTrials(change):
+    """Run N trials, either always changing doors, or always staying
 
-# Calculate stats
-fracWon = nWins / N
+    Parameters
+    ----------
+    change : bool
+        If True, change doors, otherwise stay
+    """
 
-print('Fraction of games won: ' + str(fracWon))
+    if change:
+        print('Always changing doors')
+    else:
+        print('Always staying')
+
+    print('Running simulation with N = ' + str(N) + ' trials...\n')
+
+    # Randomly assign which door the car is behind (1, 2 or 3)
+    cars = np.random.randint(1, 4, size=N)
+
+    # Randomly set the player's initial door selection (1, 2 or 3)
+    initSels = np.random.randint(1, 4, size=N)
+
+    nWins = 0
+
+    for i in range(0, N):
+        win = trial(cars[i], initSels[i], change)
+
+        if win:
+            nWins += 1
+
+    print('Number of wins = ' + str(nWins))
+
+    # Calculate stats
+    fracWon = nWins / N
+
+    print('Fraction of games won: ' + str(fracWon))
+
+
+# Run the simulation; always change doors
+runTrials(True)
+
+print('\n----------\n')
+
+# Run the simulation; always stay
+runTrials(False)
